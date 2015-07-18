d7d506af8f9cf49e43ac048c73af3d8c74659543
Kent Nagata <ngtknt@me.com>
2015-07-18 23:23:38 +0900

    名前を追加

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20150718120456) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                   limit: 255
     t.string   "email",                  limit: 255, default: "", null: false
     t.string   "encrypted_password",     limit: 255, default: "", null: false
     t.string   "reset_password_token",   limit: 255
